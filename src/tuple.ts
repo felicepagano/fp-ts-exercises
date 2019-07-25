b2ba3fb9d5a83c07baac891f3472cd03f5118a72
Felice Pagano <felice.pagano@eidosmedia.com>
2019-07-25 18:02:11 +0200

    refactor tuple map method

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,5 +1,12 @@
 export type Tuple<L, R> = [L, R];
 
+export const mapLeft = <L, M, R>(f: (l: L) => M) => (tuple: Tuple<L, R>) => [f(tuple[0]), tuple[1]];
+export const mapRight = <L, R, M>(f: (r: R) => M) => (tuple: Tuple<L, R>) => [tuple[0], f(tuple[1])];
+
 export const mapTuple = <L, M, R, S>(lf: (l: L) => M, rf: (r: R) => S) => (
   t: Tuple<L, R>
-) => [lf(t[0]), rf(t[1])];
+) => {
+  const mappedR = mapRight(rf)(t);
+  const mappedL = mapLeft(lf)(t);
+  return [mappedL[0], mappedR[1]];
+};

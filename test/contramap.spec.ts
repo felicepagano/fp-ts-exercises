79b11f4da045787ac77a9ce00661319cca4d06ad
Felice Pagano <felice.pagano@eidosmedia.com>
2019-07-25 18:02:37 +0200

    add decoder test

--- test/contramap.spec.ts
+++ test/contramap.spec.ts
@@ -1,19 +1,19 @@
 import { Predicate, contraMap } from '../src/contramap';
 
-export class Person {
-  constructor(readonly age: number, readonly name: string) {
-    this.age = age;
-    this.name = name;
+describe('Contravariant Functor', () => {
+
+  class Person {
+    constructor(readonly age: number, readonly name: string) {
+      this.age = age;
+      this.name = name;
+    }
   }
-}
 
-const felice = new Person(34, 'Felice');
+  const isAdult: Predicate<number> = age => age >= 18;
+  const checkPersonUsingIsAdult = contraMap((p: Person) => p.age)(isAdult);
 
-const isAdult: Predicate<number> = age => age >= 18;
-const checkPersonUsingIsAdult = contraMap((p: Person) => p.age)(isAdult);
-
-describe('A person of 20 years must be an adult', () => {
-  it('felice is an adult', () => {
+  it('A 34yo Person must be an adult', () => {
+    const felice = new Person(34, 'Felice');
     const isAnAdult = checkPersonUsingIsAdult(felice);
     expect(isAnAdult).toBe(true);
   });
---
+++ test/decoder.spec.ts
@@ -0,0 +1,13 @@
+import { Decoder, map } from "../src/decoder";
+
+describe('test decoder', () => {
+
+    const myLength: Decoder<number> = (s: string) => s.length;
+    const even = (n: number) => n % 2 === 0;
+
+    it('mapping a number decoder to a boolean decoder', () => {
+        const lenghtAndThenEven: Decoder<boolean> = map(even)(myLength);
+        const result = lenghtAndThenEven('Felice');
+        expect(result).toBe(true);
+      });
+});
